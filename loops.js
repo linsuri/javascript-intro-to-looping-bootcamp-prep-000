5b4993acf8f92a28d55ad6409c998857a2b29258
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-02-28 19:30:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,9 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
     if (i = 1) {
-      console.log()
+      console.log("I am 1 strange loop.")
+    } else {
+      console.log("I am ${i} strange loops.")
     }
   }
 }
